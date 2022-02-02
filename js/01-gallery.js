618d24352d7f8a21996f781885b57c6e52ca0053
Slendermanyshka <slendermanyshka@gmail.com>
2022-02-02 18:50:28 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,16 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
+const galleryList = document.querySelector('.gallery');
+const imageMarkup = createImgCard(galleryItems);
 console.log(galleryItems);
+
+galleryList.insertAdjacentHTML('beforeend',imageMarkup);
+
+createImgCard(galleryList);
+
+function createImgCard(galleryItems){
+return galleryItems.map
+
+
+
+}
